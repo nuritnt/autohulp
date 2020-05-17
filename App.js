ba805abb56f566240ef7ed0186d4551c523856a8
Tuğçe Nur Taş <tnt@panter.ch>
2020-05-17 23:53:21 +0200

    feat: clean unused code in app.js

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import 'react-native-gesture-handler';
 import * as React from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack';
+
 import Welcome from './views/Welcome.js'
 
 const Stack = createStackNavigator();
@@ -11,32 +11,9 @@ export default function App() {
   return (
     <NavigationContainer>
 
-      <Stack.Navigator>
-        <Stack.Screen
-        name="Welcome"
-        component={Welcome}
-
-        />
+      <Stack.Navigator initialRouteName="Welcome">
+        <Stack.Screen name="Welcome" component={Welcome} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
