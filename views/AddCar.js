31a028bbbfa52b8c3a48469e9612c23cda84d5a7
Tuğçe Nur Taş <tnt@panter.ch>
2020-05-18 23:17:53 +0200

    feat: update logo, input & styling

--- views/AddCar.js
+++ views/AddCar.js
@@ -1,8 +1,46 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Image, Alert, TextInput } from 'react-native';
+import { TouchableHighlight } from 'react-native-gesture-handler';
+import globalStyles from '../globalStyles';
 
 const styles = StyleSheet.create({
-  container: {}
+  container: {
+    flex: 1,
+    backgroundColor: '#F2FFFF',
+    paddingVertical: 80,
+    paddingHorizontal: 50,
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  },
+  image: {},
+  text: {
+    fontSize: 32,
+    lineHeight: 39,
+  },
+  inputWrapper:{
+    width: '100%'
+  },
+  inputLabel:{
+    color: '#989898',
+    fontSize: 14,
+    lineHeight: 17,
+    textAlign: 'left',
+    marginBottom: 6,
+  },
+  textInput: {
+    color: '#000000',
+    fontSize: 24,
+    lineHeight: 29,
+    borderColor: '#BFBFBF',
+    borderWidth: 2,
+    width: '100%',
+    height: 47,
+    padding: 10,
+    marginBottom: 16,
+  },
+  buttonWrapper: {
+    width: '100%'
+  },
 });
 
 export default class AddCar extends Component {
@@ -10,7 +48,43 @@ export default class AddCar extends Component {
     const { navigate } = this.props.navigation
     return (
       <View style={styles.container}>
-        <Text>Hello me worky</Text>
+        <Image style={styles.image} source={require('../img/icon.png')} />
+        <Text style={styles.text}>Add New Car</Text>
+
+        <View style={styles.inputWrapper}>
+          <Text style={styles.inputLabel}>Description</Text>
+          <TextInput
+            style={styles.textInput}
+            placeholder={'Saable'}
+            placeholderTextColor={'#989898'}
+          >
+          </TextInput>
+          <Text style={styles.inputLabel}>License Plate</Text>
+          <TextInput
+            style={styles.textInput}
+            placeholder={'ZH123456'}
+            placeholderTextColor={'#989898'}
+          ></TextInput>
+          <Text style={styles.inputLabel}>Autohulp Car Code</Text>
+          <TextInput
+            style={styles.textInput}
+            placeholder={'xh3dc39'}
+            placeholderTextColor={'#989898'}
+          ></TextInput>
+        </View>
+
+        <View style={styles.buttonWrapper}>
+          <TouchableHighlight
+            style={globalStyles.button}
+            underlayColor={'#FFFFFF'}
+            onPress={() => Alert.alert('Car added')}
+          >
+            <Text style={globalStyles.buttonText}>
+              Add Car
+            </Text>
+          </TouchableHighlight>
+        </View>
+
       </View>
     )
   }
